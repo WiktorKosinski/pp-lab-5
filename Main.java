808bba030df1d1a4d3fc3c03ee47d9acbdf3113c
Wiktor Kosiński <wiki.kosinski@gmail.com>
2024-05-04 12:24:36 +0200

    zad4

--- Main.java
+++ Main.java
@@ -1,12 +1,30 @@
+import company.beans.Person;
+import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
-        int result = MathUtils.add(5,10);
-        System.out.println("Result of addition: " + result);
+        Person[] people = new Person[5];
+        final int b = 10;
+
+        try {
+            people[0] = new Person("Jude Bellingham", 20);
+            people[1] = new Person("Cristiano Ronaldo", 38);
+            people[2] = new Person("George Floyd", 45);
+            people[3] = new Person("Lebron James", 39);
+            people[4] = new Person("Luka Modric", 38);
+        } catch (InvalidAgeException e) {
+            System.out.println("Invalid age provided: " + e.getMessage());
+        }
 
         EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("The result of addition is: " + result);
+
+        for (Person person : people) {
+            if (person != null) {
+                int value = MathUtils.add(person.getAge(), b);
+                emailMessenger.sendMessage("Hello " + person.getName() + ", the value is: " + value);
+            }
+        }
     }
 }
